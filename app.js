0b2245bbbaee7ea814cccd8c0984d3647ed7b851
mizanmahi <mizanmahi24@gmail.com>
2020-08-20 16:27:19 +0600

    partials

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.use(shopRouter);
 // 404 page handler
 app.use((req, res) => {
     res.status(404).render('404', {
-        pageTitle: 'Page Not Found'
+        pageTitle: 'Page Not Found',
+        path: ''
     })
 })
 
